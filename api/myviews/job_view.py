34aaf63e0633889a4a57a636c0c0c47e68a2a0b8
Your Name <you@example.com>
2020-04-08 21:30:42 -0300

    update hateoas bug

--- api/myviews/job_view.py
+++ api/myviews/job_view.py
@@ -1,5 +1,5 @@
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
--- api/serializers/job_serializer.py
+++ api/serializers/job_serializer.py
@@ -3,18 +3,18 @@ from ..models import Job
 
 
 class JobSerializer(serializers.ModelSerializer):
-    links = serializers.SerializerMethodField()
+    # links = serializers.SerializerMethodField()
 
     class Meta:
         model = Job
         fields = ('title', 'description', 'salary', 'company_name',
-                  'amount_of_jobs', 'contact', 'type_of_job', 'technologies', 'links',)
+                  'amount_of_jobs', 'contact', 'type_of_job', 'technologies',)
 
-    def get_links(self, obj):
-        request = self.context['request']
-        return {
-            'self': reverse.reverse('job-list-id', kwargs={'id': obj.pk}, request=request),
-            'delete': reverse.reverse('job-list-id', kwargs={'id': obj.pk}, request=request),
-            'update': reverse.reverse('job-list-id', kwargs={'id': obj.pk}, request=request),
-        }
+    # def get_links(self, obj):
+    #     request = self.context['request']
+    #     return {
+    #         'self': reverse.reverse('job-list-id', kwargs={'id': obj.pk}, request=request),
+    #         'delete': reverse.reverse('job-list-id', kwargs={'id': obj.pk}, request=request),
+    #         'update': reverse.reverse('job-list-id', kwargs={'id': obj.pk}, request=request),
+    #     }
 
